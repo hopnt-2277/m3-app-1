429c098e2cdc60f0d50205998c60577ef555ebb2
hopnt98@gmail.com <hopnt98@gmail.com>
2023-12-21 08:45:07 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +11,24 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
+
+  
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Set 'name' to the text in the 'name_box'
+    name = self.name_box.text
+    # Set 'email' to the text in the 'email_box'
+    email = self.email_box.text
+    # Set 'feedback' to the text in the 'feedback_box'
+    feedback = self.feedback_box.text
+    anvil.server.call('add_feedback', name, email, feedback)
+    # Show a popup that says 'Feedback submitted!'
+    Notification("Feedback submitted!").show()
+    # Call your 'clear_inputs' method to clear the boxes
+    self.clear_inputs()
